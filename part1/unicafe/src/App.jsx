8cc5f58fb4404b48709dd46d1ee73c85d1f746c7
Jared Klopstein <jaredklopstein@gmail.com>
2025-03-09 19:35:17 -0500

    completed 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,18 +19,27 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
+      <table>
+        <tbody>
       <StatisticLine text='good' value={good}/>
       <StatisticLine text='neutral' value={neutral}/>
       <StatisticLine text='bad' value={bad}/>
       <StatisticLine text='all' value={total}/>
       <StatisticLine text='average' value={average}/>
       <StatisticLine text='positive' value={positive}/>
+        </tbody>
+      </table>
     </div>
   )
 }
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
 const App = () => {
   const [good, setGood] = useState(0);
